babf640f8186c0a80ce892891ee9f58b4ff05cf7
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-11 16:40:25 -0700

    Finish form functionality

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,71 +1,75 @@
-import React, { useState } from 'react'
-import { Axios, db } from '../../firebase/firebaseConfig'
+import React from "react";
 import './SignUp.scss'
 
-const SignUp = () => {
-  const [formData, setFormData] = useState({})
-
-  const updateInput = e => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    })
-  }
-  const handleSubmit = event => {
-    event.preventDefault()
-    sendEmail()
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    })
-  }
-  const sendEmail = () => {
-    Axios.post(
-      'https://us-central1-metzger-website.cloudfunctions.net/submit',
-      formData
-    )
-      .then(res => {
-        db.collection('emails').add({
-          name: formData.name,
-          email: formData.email,
-          message: formData.message,
-          time: new Date(),
-        })
-      })
-      .catch(error => {
-        console.log(error)
-      })
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
   }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={updateInput}
-          value={formData.name || ''}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={updateInput}
-          value={formData.email || ''}
-        />
-        <textarea
-          type="text"
-          name="message"
-          placeholder="Message"
-          onChange={updateInput}
-          value={formData.message || ''}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  )
+  render() {
+    const { status } = this.state;
+    return (
+      <div class="signUp">
+        <div class="row">
+          <h1>Contact Me</h1>
+        </div>
+        <div class="row">
+          <h4>I'd love to hear from you!</h4>
+        </div>
+        <div class="row input-container">
+          <div class="col-xs-12">
+            <div class="styled-input wide">
+              <input type="text" required />
+              <label>Name</label>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <div class="styled-input">
+              <input type="text" required />
+              <label>Email</label>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <div class="styled-input">
+              <input type="text" required />
+              <label>Phone Number</label>
+            </div>
+          </div>
+          <div class="col-xs-12">
+            <div class="styled-input wide">
+              <textarea required></textarea>
+              <label>Message</label>
+            </div>
+          </div>
+          <div class="col-xs-12">
+            {status === "SUCCESS" ? <p>Thanks!</p> : <div class="btn-lrg submit-btn"><button>Submit</button></div>}
+            {status === "ERROR" && <p>Ooops! There was an error.</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
 }
-
-export default SignUp
