46b79a128f1425d2a207d4deb48421b6f9780225
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-12 14:46:27 -0700

    Add styling adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import Logo from '../images/logo.png'
 import './Navbar.css';
 
+const scrollToRef = (ref) => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 
 function Navbar() {
+  const myRef = useRef(null)
+  const executeScroll = () => scrollToRef(myRef)
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
@@ -30,18 +33,22 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            <img src={Logo} alt="KM logo" height="140px" />
-          </Link>
+          <div ref={myRef}>
+            <Link to='/' className='navbar-logo' onClick={executeScroll}>
+              <img src={Logo} alt="KM logo" height="140px" />
+            </Link>
+          </div>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
+            <div ref={myRef}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={executeScroll}>
+                  Home
               </Link>
-            </li>
+              </li>
+            </div>
             <li className='nav-item'>
               <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
                 About Me
