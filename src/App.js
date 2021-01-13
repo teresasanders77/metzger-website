c99fc2d690e23041461c5660c736686c94bb777b
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-13 09:52:19 -0700

    Fixed background image responsive issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
