69be71f550516b7081b7bf6376b0d26ef84f22d8
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-26 13:26:02 -0700

    Add questionnaire.js

---
+++ src/components/pages/Questionnaire.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Questionnaire.css';
+import 'survey-react/survey.css';
+import * as Survey from 'survey-react';
+
+class Questionnaire extends Component {
+  render() {
+    return (
+      <div className="Survey">
+        <h1>HELLO</h1>
+      </div>
+    )
+  }
+}
+
+export default Questionnaire;
