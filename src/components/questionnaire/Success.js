f4e7c87c5fbcb27211e898cc1adbea7a5932bb30
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-28 16:39:54 -0700

    Finished styling success page

--- src/components/questionnaire/Success.js
+++ src/components/questionnaire/Success.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(83, 108, 140)'
+    }
+  },
   status: {
     danger: orange[500],
   },
@@ -24,15 +32,25 @@ export class Success extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <>
-          <Dialog
-            open
-            fullWidth
-            maxWidth='sm'
-          >
-            <AppBar title="Success" />
-            <h1>Thanks for your Submission</h1>
-            <p>You will get email with further instruction</p>
-          </Dialog>
+          <Box px={55} my={40}>
+            <AppBar
+              title="Success"
+              position="static"
+              style={{ background: 'rgb(83, 108, 140)' }}>
+              <Toolbar variant="dense">
+                <Typography
+                  variant="h6"
+                  style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                  }}
+                >
+                  <h1>Thanks for your Submission</h1>
+                  <p>You will get email an with further instruction</p>
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Box>
         </>
       </MuiThemeProvider>
     )
