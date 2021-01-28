cd23528309b172a5e18129993aff243f112087b7
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-28 15:51:41 -0700

    Finished styling step 4

--- src/components/questionnaire/HighPerformance.js
+++ src/components/questionnaire/HighPerformance.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import FormLabel from '@material-ui/core/FormLabel';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(83, 108, 140)'
+    }
+  },
   status: {
     danger: orange[500],
   },
@@ -29,113 +36,132 @@ export class HighPerformance extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <>
-          <Dialog
-            open
-            fullWidth
-            maxWidth='sm'
-          >
-            <AppBar position="static">
+          <Box px={55} my={3}>
+            <AppBar
+              position="static"
+              style={{ background: 'rgb(83, 108, 140)' }}
+            >
               <Toolbar variant="dense">
-                QUESTIONNAIRE FOR YOUR COACHING STRATEGY SESSION
-                            </Toolbar>
+                <Typography
+                  variant="h6"
+                  style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                  }}
+                >
+                  QUESTIONNAIRE FOR YOUR COACHING STRATEGY SESSION
+                </Typography>
+              </Toolbar>
             </AppBar>
+            <br /><br /><br />
+            <FormLabel component="legend">What negative recurring thoughts, fears, or behaviors would you like to overcome in order to feel more psychologically free,
+              confident and successful?</FormLabel>
             <TextField
-              placeholder="Negative"
-              label="What negative recurring thoughts, fears, or behaviors would you like to overcome in order to feel more psychologically free,
-              confident and successful?"
               onChange={handleChange('negative')}
               defaultValue={values.negative}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">What else has prevented you from having that dream life?</FormLabel>
             <TextField
-              placeholder="Prevent"
-              label="What else has prevented you from having that dream life?"
               onChange={handleChange('prevent')}
               defaultValue={values.prevent}
               margin="normal"
               fullWidth
-
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">What eating, exercise or general health habits would you like to begin or break in order to feel stronger and more healthy
+              physiologically?</FormLabel>
             <TextField
-              placeholder="Habits"
-              label="What eating, exercise or general health habits would you like to begin or break in order to feel stronger and more healthy
-              physiologically?"
               onChange={handleChange('habits')}
               defaultValue={values.habits}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">What distracts you the most from being more productive, and what major projects or missions are you struggling to complete
+              faster or more efficiently?</FormLabel>
             <TextField
-              placeholder="Distractions"
-              label="What distracts you the most from being more productive, and what major projects or missions are you struggling to complete
-              faster or more efficiently?"
               onChange={handleChange('distractions')}
               defaultValue={values.distractions}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">If you were more persuasive or influential, what dream or desire would you ask others to support you in achieving?</FormLabel>
             <TextField
-              placeholder="Desire"
-              label="If you were more persuasive or influential, what dream or desire would you ask others to support you in achieving?"
               onChange={handleChange('desire')}
               defaultValue={values.desire}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">When do you struggle to be fully present in your day or in any of your relationships?</FormLabel>
             <TextField
-              placeholder="Struggle"
-              label="When do you struggle to be fully present in your day or in any of your relationships?"
               onChange={handleChange('struggle')}
               defaultValue={values.struggle}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">How purposeful do you feel in living each day, and how would you describe your purpose?</FormLabel>
             <TextField
-              placeholder="Purposeful"
-              label="How purposeful do you feel in living each day, and how would you describe your purpose?"
               onChange={handleChange('purposeful')}
               defaultValue={values.purposeful}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">Why would you like to work with a high performance coach?</FormLabel>
             <TextField
-              placeholder="Why"
-              label="Why would you like to work with a high performance coach?"
               onChange={handleChange('whyCareerCoach')}
               defaultValue={values.whyCareerCoach}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <br />
+            <br /><br />
+            <FormLabel component="legend">Why do you feel you could be a great student or coaching client if we worked together?</FormLabel>
             <TextField
-              placeholder="Why You"
-              label="Why do you feel you could be a great student or coaching client if we worked together?"
               onChange={handleChange('whyStudent')}
               defaultValue={values.whyStudent}
               margin="normal"
               fullWidth
+              multiline
+              variant="outlined"
             />
-            <Button
-              size="large"
-              color="secondary"
-              variant="contained"
-              onClick={this.continue}
-            >Continue</ Button>
-            <Button
-              size="large"
-              color="secondary"
-              variant="contained"
-              onClick={this.back}
-            >Back</ Button>
-          </Dialog>
+            <Box textAlign='center'>
+              <Button
+                size="large"
+                color="main"
+                variant="contained"
+                onClick={this.back}
+              >Back</ Button>
+              &nbsp;&nbsp;&nbsp;
+              <Button
+                size="large"
+                color="main"
+                variant="contained"
+                onClick={this.continue}
+              >Continue</ Button>
+            </Box>
+          </Box>
         </>
       </MuiThemeProvider>
     )
