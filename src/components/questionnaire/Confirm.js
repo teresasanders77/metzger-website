ac2e8b420f2d0f551f4bf698422521f2a7ed49fb
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-01-28 16:34:59 -0700

    Update information for confirmation

--- src/components/questionnaire/Confirm.js
+++ src/components/questionnaire/Confirm.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
-import { List, ListItem, ListItemText } from '@material-ui/core/';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { List, ListItem, ListItemText } from '@material-ui/core/';
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(83, 108, 140)'
+    }
+  },
   status: {
     danger: orange[500],
   },
@@ -24,24 +31,81 @@ export class Confirm extends Component {
     this.props.prevStep();
   }
   render() {
-    const { values: { firstName, lastName, email, occupation, city, bio } } = this.props;
+    const { values: {
+      fullName,
+      phoneNumber,
+      address,
+      cityStateZip,
+      country,
+      email,
+      occupation,
+      dateOfBirth,
+      gender,
+      marriageStatus,
+      description,
+      clarity,
+      energy,
+      courage,
+      productivity,
+      influence,
+      doForLiving,
+      threeGoals,
+      stressors,
+      successful,
+      dreamLife,
+      prevent,
+      goalFail,
+      threeChanges,
+      proud,
+      negative,
+      habits,
+      distractions,
+      desire,
+      struggle,
+      purposeful,
+      whyCareerCoach,
+      whyStudent
+    } } = this.props;
     return (
       <MuiThemeProvider theme={theme}>
         <>
-          <Dialog
-            open
-            fullWidth
-            maxWidth='sm'
-          >
-            <AppBar title="Confirm User Data" />
+          <Box px={55} my={3}>
+            <AppBar
+              position="static"
+              style={{ background: 'rgb(83, 108, 140)' }}
+            >
+              <Toolbar variant="dense">
+                <Typography
+                  variant="h6"
+                  style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                  }}
+                >
+                  CONFIRM QUESTIONNAIRE DATA
+                </Typography>
+              </Toolbar>
+            </AppBar>
             <List>
               <ListItem>
-                <ListItemText primary="First Name"
-                  secondary={firstName} />
+                <ListItemText primary="Full Name"
+                  secondary={fullName} />
               </ListItem>
               <ListItem>
-                <ListItemText primary="Last Name"
-                  secondary={lastName} />
+                <ListItemText primary="Phone Number"
+                  secondary={phoneNumber} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Address"
+                  secondary={address} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="City, State, Zip"
+                  secondary={cityStateZip} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Country"
+                  secondary={country} />
               </ListItem>
               <ListItem>
                 <ListItemText primary="Email"
@@ -52,28 +116,135 @@ export class Confirm extends Component {
                   secondary={occupation} />
               </ListItem>
               <ListItem>
-                <ListItemText primary="City"
-                  secondary={city} />
+                <ListItemText primary="Date of Birth"
+                  secondary={dateOfBirth} />
               </ListItem>
               <ListItem>
-                <ListItemText primary="Bio"
-                  secondary={bio} />
+                <ListItemText primary="Gender"
+                  secondary={gender} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Marriage Status"
+                  secondary={marriageStatus} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Please briefly describe how you heard about my coaching services and why you would like a Strategy Session with me:"
+                  secondary={description} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Do you feel you are clear about who you are, your purpose, and the direction you want to
+go in life?"
+                  secondary={clarity} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Do you consistently have enough mental and physical energy needed to excel, accomplish
+your goals, and feel motivated and happy?"
+                  secondary={energy} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Do you take action and consistently express who you truly are and what you truly think,
+need, and desire with the world?"
+                  secondary={courage} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Are you consistently focused and effective, and are you good at minimizing distractions
+and maintaining priorities?"
+                  secondary={productivity} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Do you feel you have the social influence with your family, friends, and team needed to
+accomplish your goals?"
+                  secondary={influence} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What do you do for a living, and why did you choose that career?"
+                  secondary={doForLiving} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What are your top 3 goals you are striving to achieve right now?"
+                  secondary={threeGoals} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What major stressors or challenges are you struggling with right now?"
+                  secondary={stressors} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="When you feel like your most successful and happy self, what makes you feel that way?"
+                  secondary={successful} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What would your dream life look like if you could wave a wand and make it happen?"
+                  secondary={dreamLife} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What else has prevented you from having that dream life?"
+                  secondary={prevent} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What goal or dream have you ever given up on or failed at, and what do you believe caused you to stop or fail?"
+                  secondary={goalFail} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What 3 big changes would you like to make in your life in the next 12 months?"
+                  secondary={threeChanges} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What are you most proud of and excited about in your life?"
+                  secondary={proud} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What negative recurring thoughts, fears, or behaviors would you like to overcome in order to feel more psychologically free,
+confident and successful?"
+                  secondary={negative} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What eating, exercise or general health habits would you like to begin or break in order to feel stronger and more healthy
+physiologically?"
+                  secondary={habits} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="What distracts you the most from being more productive, and what major projects or missions are you struggling to complete
+faster or more efficiently?"
+                  secondary={distractions} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="If you were more persuasive or influential, what dream or desire would you ask others to support you in achieving?"
+                  secondary={desire} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="When do you struggle to be fully present in your day or in any of your relationships?"
+                  secondary={struggle} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="How purposeful do you feel in living each day, and how would you describe your purpose?"
+                  secondary={purposeful} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Why would you like to work with a high performance coach?"
+                  secondary={whyCareerCoach} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Why do you feel you could be a great student or coaching client if we worked together?"
+                  secondary={whyStudent} />
               </ListItem>
             </List>
             <br />
-            <Button
-              size="large"
-              color="secondary"
-              variant="contained"
-              onClick={this.continue}
-            >Confirm & Continue </Button>
-            <Button
-              size="large"
-              color="secondary"
-              variant="contained"
-              onClick={this.back}
-            >Back</Button>
-          </Dialog>
+            <Box textAlign='center'>
+              <Button
+                size="large"
+                color="main"
+                variant="contained"
+                onClick={this.back}
+              >Back</Button>
+             &nbsp;&nbsp;&nbsp;
+             <Button
+                size="large"
+                color="main"
+                variant="contained"
+                onClick={this.continue}
+              >Confirm & Continue </Button>
+            </Box>
+          </Box>
         </>
       </MuiThemeProvider>
     )
